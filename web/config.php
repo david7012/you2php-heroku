a5bf88b6686017324f7d059fdf6ea0f87ef0179d
david7012 <37462500+david7012@users.noreply.github.com>
2018-03-18 08:56:54 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyDZ9v5F8syw1kYAUrxqQMXJq6CFGb_1B-c');//这里改成您的api key
+define('GJ_CODE', 'JP');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EN2DEKEY', 'sfdsdfg323467gn');//一个随机字符串，加解密KEY
+define('EMAIL', 'cxw7012@gmail.com');//邮箱
 ?>
